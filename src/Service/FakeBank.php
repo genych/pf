6264dc98a1c03d3f52893d93d9a8336942f1bccc
genych <evgenyshe+pf@gmail.com>
2020-07-31 20:15:27 +0300

    cleanup

--- src/Service/FakeBank.php
+++ src/Service/FakeBank.php
@@ -4,29 +4,17 @@ namespace App\Service;
 
 use App\Entity\Customer;
 use App\Entity\Order;
-use App\Repository\CustomerRepository;
 
 class FakeBank
 {
     const INITIAL_BALANCE = 10000;
 
-    /** @var CustomerRepository */
-    private CustomerRepository $customerRepository;
-
-    /**
-     * @param CustomerRepository $customerRepository
-     */
-    public function __construct(CustomerRepository $customerRepository)
+    public static function fromMinorUnits(?int $amount): string
     {
-        $this->customerRepository = $customerRepository;
+        return sprintf("%.2f", round((int)$amount / 100, 2));
     }
 
-    public static function fromMinorUnits(int $amount): string
-    {
-        return sprintf("%.2f", round($amount / 100, 2));
-    }
-
-    public function getBalance(Customer $customer): int
+    public static function getBalance(Customer $customer): int
     {
         $prices = $customer->getOrders()->map(function(Order $o) {return $o->getPrice();});
         return self::INITIAL_BALANCE - array_sum($prices->toArray());
